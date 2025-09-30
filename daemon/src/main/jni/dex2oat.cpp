a22c685502c1f0fc73a54c9c31def6b17f749cd7
rikka <ayasa0520@gmail.com>
2025-09-30 23:58:50 +0800

    test
    test

--- daemon/src/main/jni/dex2oat.cpp
+++ daemon/src/main/jni/dex2oat.cpp
@@ -72,7 +72,7 @@ extern "C" JNIEXPORT void JNICALL Java_org_lsposed_lspd_service_Dex2OatService_d
             if (d32) umount(d32p);
             if (r64) umount(r64p);
             if (d64) umount(d64p);
-            execlp("resetprop", "resetprop", "dalvik.vm.dex2oat-flags", "--inline-max-code-units=0",
+            execlp("resetprop", "resetprop", "--", "dalvik.vm.dex2oat-flags", "--inline-max-code-units=0",
                    nullptr);
         }
 
